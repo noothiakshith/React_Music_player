eca96efacaec9d4d906414245d582cab565ad4df
Noothi Akshith <nakshith_csb223244@mgit.ac.in>
2024-03-19 18:01:39 +0530

    Update .env.example and Discover.jsx, add shazamCoreApi to store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { shazamCoreApi } from './Services/ShazamCore';
 
 import playerReducer from './features/playerSlice';
 
 export const store = configureStore({
   reducer: {
+    [shazamcCoreApi.reducerpath]:shazamcCoreApi.reducer,
     player: playerReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(shazamcCoreApi.middleware),
 });
